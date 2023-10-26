de456c009305f3c7758070ea831ae1261b5403d0
Shawee <757221692@qq.com>
2023-10-26 23:09:00 +0800

    配置mybaitis连接

--- product/src/test/java/com/atguigu/mall/product/ProductApplicationTests.java
+++ product/src/test/java/com/atguigu/mall/product/ProductApplicationTests.java
@@ -1,13 +1,27 @@
 package com.atguigu.mall.product;
 
+import com.atguigu.mall.product.entity.BrandEntity;
+import com.atguigu.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ProductApplicationTests {
 
+    @Resource
+    private BrandService brandService;
+
+
     @Test
-    void contextLoads() {
+    public void testInsertBrand() {
+        BrandEntity brandEntity = new BrandEntity();
+        brandEntity.setName("华为");
+        brandService.save(brandEntity);
     }
 
 }
